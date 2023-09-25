b16c47ed4df004adc96e154cebfd9640b5def7a8
Yamato Semling <yamato.semling@bict-edu.ch>
2023-09-25 16:30:24 +0200

    commit14

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const body = document.body;
 
+const main = body.querySelector("main");
+const loading = body.querySelector("#loading");
+
 const fortschritt = document.querySelector("#fortschritt");
 const tradition = document.querySelector("#tradition");
 const freiheit = document.querySelector("#freiheit");
@@ -18,7 +21,12 @@ const freiImg = document.createElement("img");
 freiImg.src = "../img/icon/3.png";
 freiImg.classList.add("icon");
 
-//changes the image on rightCircle
+window.addEventListener("load", (event) => {
+  loading.style.display = "none";
+  main.style.display = "block";
+});
+
+//changes the image on .rightCircle
 const changeIcon = function (id) {
   switch (id) {
     case 1:
