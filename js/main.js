3bb4b784be1551f48295a49118926894d0088f58
Yamato Semling <yamato.semling@bict-edu.ch>
2023-09-27 16:37:03 +0200

    commit18

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ window.addEventListener("load", (event) => {
   loading.style.display = "none";
   main.style.display = "block";
 });
+
+const menu = document.getElementsByClassName("menu-links");
+
+const uncheck = function () {
+  const checkbox = body.querySelector(".checkbox");
+  checkbox.checked = false;
+};
+
+const listeners = function () {
+  for (var i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", uncheck);
+  }
+};
+
+listeners();
